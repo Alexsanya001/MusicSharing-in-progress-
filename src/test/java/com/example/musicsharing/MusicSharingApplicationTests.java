1f788ba5627332b02f110359f89734dec5a4dbe8
alexsanya001 <alexsanya001@gmail.com>
2024-12-15 13:24:53 +0200

    Initial commit

---
+++ src/test/java/com/example/musicsharing/MusicSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
