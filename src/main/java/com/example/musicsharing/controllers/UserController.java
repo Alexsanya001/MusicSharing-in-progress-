0e6b248c8fb2b3d792fdcfd592d6c0fbbb93164a
alexsanya001 <alexsanya001@gmail.com>
2024-12-19 17:17:27 +0200

    Simple security realized

--- src/main/java/com/example/musicsharing/controllers/UserController.java
+++ src/main/java/com/example/musicsharing/controllers/UserController.java
@@ -5,11 +5,13 @@ import com.example.musicsharing.models.dto.UserInfoDTO;
 import com.example.musicsharing.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,6 +26,16 @@ public class UserController {
         UserInfoDTO user = userService.showUser(principal.getName());
 
         ApiResponse<UserInfoDTO> response = ApiResponse.success(user);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<List<UserInfoDTO>>> getAllUsers() {
+
+        List<UserInfoDTO> users = userService.getAllUsers();
+        ApiResponse<List<UserInfoDTO>> response = ApiResponse.success(users);
 
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/example/musicsharing/services/UserService.java
+++ src/main/java/com/example/musicsharing/services/UserService.java
@@ -5,10 +5,14 @@ import com.example.musicsharing.models.dto.RegisterDTO;
 import com.example.musicsharing.models.dto.UserInfoDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
     long createUser(RegisterDTO registerDTO);
 
     String createTokenOnLogin(LoginDTO loginDTO);
 
     UserInfoDTO showUser(String username);
+
+    List<UserInfoDTO> getAllUsers();
 }
