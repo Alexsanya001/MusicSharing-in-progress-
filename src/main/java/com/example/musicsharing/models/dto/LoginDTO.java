c065647e2293bf5b24cb5e9fcd501b7899780833
alexsanya001 <alexsanya001@gmail.com>
2024-12-21 10:43:49 +0200

    Register and login in AuthController tested.

--- src/main/java/com/example/musicsharing/models/dto/LoginDTO.java
+++ src/main/java/com/example/musicsharing/models/dto/LoginDTO.java
@@ -1,10 +1,12 @@
 package com.example.musicsharing.models.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class LoginDTO {
     private String username;
     private String password;
---
+++ src/test/java/com/example/musicsharing/controllers/AuthControllerTest.java
@@ -0,0 +1,123 @@
+package com.example.musicsharing.controllers;
+
+import com.example.musicsharing.models.dto.ApiResponse;
+import com.example.musicsharing.models.dto.LoginDTO;
+import com.example.musicsharing.models.dto.RegisterDTO;
+import com.example.musicsharing.repositories.UserRepository;
+import com.example.musicsharing.services.UserService;
+import com.example.musicsharing.util.JWTUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+    @MockitoBean
+    private JWTUtil jwtUtil;
+    @MockitoBean
+    private UserRepository userRepository;
+
+
+    @Test
+    void register_returnsLocation_whenUserIsRegistered() throws Exception {
+        RegisterDTO registerDTO = RegisterDTO.builder()
+                .username("Valid_username")
+                .password("Valid_password1")
+                .email("Valid@email.com")
+                .firstName("Valid First name")
+                .lastName("Valid Last name")
+                .build();
+
+        long userId = 1L;
+        String requestBody = objectMapper.writeValueAsString(registerDTO);
+
+        when(userRepository.existsByUsername(registerDTO.getUsername()))
+                .thenReturn(false);
+        when(userRepository.existsByEmail(registerDTO.getEmail()))
+                .thenReturn(false);
+        when(userService.createUser(any(RegisterDTO.class))).thenReturn(userId);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                ).andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/users/" + userId));
+    }
+
+
+    @Test
+    void register_returnsFailure_whenFieldsAreNotValid() throws Exception {
+        RegisterDTO registerDTO = RegisterDTO.builder()
+                .username(" ")
+                .password("invalid_password")
+                .email("not-valid_email.com")
+                .firstName("Invalid1")
+                .lastName("Invalid2")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(registerDTO);
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors.size()").value(5));
+
+        verify(userService, never()).createUser(any());
+    }
+
+
+    @Test
+    void login_shouldReturnToken_whenUserIsAuthenticated() throws Exception {
+        LoginDTO loginDTO = LoginDTO.builder()
+                .username("username")
+                .password("password")
+                .build();
+        String token = "token";
+        Authentication authentication = mock(Authentication.class);
+        ApiResponse<String> response = ApiResponse.success(token);
+        String requestBody = objectMapper.writeValueAsString(loginDTO);
+        String expectedJson = objectMapper.writeValueAsString(response);
+
+        when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(authentication);
+        when(userService.createTokenOnLogin(any(LoginDTO.class))).thenReturn(token);
+
+        mockMvc.perform(post("/api/auth/login")
+        .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
+
+    }
+}
