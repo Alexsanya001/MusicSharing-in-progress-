d923c172b42fa445124199f52d3dbb8c5a5f3928
alexsanya001 <alexsanya001@gmail.com>
2025-03-13 11:34:07 +0200

    Migrations for music tables

---
+++ src/main/resources/db/migration/structure/V20250313104401__create_tables_music.sql
@@ -0,0 +1,24 @@
+-- Music metadata
+CREATE TABLE IF NOT EXISTS music
+(
+    id        BIGSERIAL PRIMARY KEY,
+    title     VARCHAR NOT NULL,
+    price     DECIMAL(10, 2),
+    author_id BIGINT  NOT NULL REFERENCES users (id) ON DELETE CASCADE
+);
+
+-- Music files
+CREATE TABLE IF NOT EXISTS music_files
+(
+    id       BIGSERIAL PRIMARY KEY,
+    file_url TEXT UNIQUE   NOT NULL,
+    music_id BIGINT UNIQUE NOT NULL REFERENCES music (id) ON DELETE CASCADE
+);
+
+-- Purchased tracks
+CREATE TABLE IF NOT EXISTS purchases
+(
+    music_id BIGINT NOT NULL REFERENCES music (id) ON DELETE CASCADE,
+    user_id  BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, music_id)
+);
---
+++ src/main/resources/db/migration/structure/V20250313112502__create_indexes_music.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_music_author_id ON music(author_id);
+CREATE INDEX idx_music_files_music_id ON music_files(music_id);
